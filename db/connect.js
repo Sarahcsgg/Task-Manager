0159a63d855e512ff91283c65d8255a937cd0e88
Sarah Guimarães <sarahguimaraes89@gmail.com>
2023-11-28 15:51:04 -0300

    Second large commit

--- db/connect.js
+++ db/connect.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose')
 
-const connectionString = 'mongodb+srv://sarahguimaraes89:123456@taskmanager.1cm6px3.mongodb.net/?retryWrites=true&w=majority'
 
-mongoose.connect(connectionString, {
-    useNewUrlParser: true, 
-    useCreateIndex: true,
-})
-.then(()=> console.log('CONNECT TO THE DB..'))
-.catch((err)=>console.log(err))
+const connectDB = (url) => {
+    return mongoose.connect(url, {
+        useNewUrlParser: true, 
+        useCreateIndex: true,
+})}
+
+module.exports = connectDB
+
+// mongoose.connect(connectionString, {
+//     useNewUrlParser: true, 
+//     useCreateIndex: true,
+// })
+// .then(()=> console.log('CONNECT TO THE DB..'))
+// .catch((err)=>console.log(err))
 
